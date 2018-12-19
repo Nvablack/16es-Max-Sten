efad204ee7f1445e622e5f14abfde4b69b533ec5
Nvablack <45422136+Nvablack@users.noreply.github.com>
2018-12-19 14:16:01 +0100

    Create Scanner(Broken).java

---
+++ Scanner(Broken).java
@@ -0,0 +1,50 @@
+﻿import java.util.Scanner;
+
+public class Main {
+
+
+public static void main(String[] args){
+
+
+
+
+Scanner keyboard = new Scanner(System.In);
+
+System.out.print('something ');
+
+String fromKeyboard = keyboard.nextLine();
+
+Sytem.out.println('Orginal:'+ fromKeyboard);
+
+System.out.println('VERSALER:'+ fromKeyboard.toUpperCase());
+
+System.out.println('Gemener:'+ fromKeyboard.toLowerCase());
+
+System.out.println('Stränglängd'+ fromKeyboard.lenght()+ 'tecken');
+
+
+
+for (int i=0; i<fromKeyboard.lenght(); i++){
+	System.out.println(fromKeyboard.charAt(i));
+
+
+}
+for (int position = fromKeyboard.lenght(); position >= 0; position--){
+	System.out.print(fromKeyboard.charAt(position));
+
+	
+}
+
+Sysyen.out.println();
+
+
+
+
+
+
+
+
+
+
+}
+}
